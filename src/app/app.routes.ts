45eacd7be8ea29f09d3832d5150b4a011e356acd
Anupam Ghosh <anupamghosh@N-Interns-224-AnupamGhosh.local>
2025-06-26 00:02:56 +0530

    fixed the hero section and added scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,19 @@ import { OrderPageComponent } from './modules/order/order-page.component';
 import { InvoicePageComponent } from './modules/invoice/invoice-page.component';
 import { AuthPageComponent } from './modules/auth/auth-page.component';
 import { TestPageComponent } from './modules/test/test-page.component';
+import { DashboardPageComponent } from './modules/dashboard/dashboard-page.component';
+import { InventoryPageComponent } from './modules/inventory/inventory-page.component';
 
 export const routes: Routes = [
+  { path: 'dashboard', component: DashboardPageComponent },
   { path: 'client', component: ClientPageComponent },
   { path: 'product', component: ProductPageComponent },
   { path: 'order', component: OrderPageComponent },
   { path: 'invoice', component: InvoicePageComponent },
+  { path: 'inventory', component: InventoryPageComponent },
   { path: 'auth', component: AuthPageComponent },
   { path: 'test', component: TestPageComponent },
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth' }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' }
+
 ];
