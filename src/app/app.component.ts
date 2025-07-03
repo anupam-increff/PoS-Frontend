4edd9e72c5d67cc67501a51d390e98f5080bfb81
Anupam Ghosh <anupamghosh@N-Interns-224-AnupamGhosh.local>
2025-07-03 16:49:53 +0530

    maintaining global scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet  , RouterLink} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -7,7 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule, ReactiveFormsModule],
+  imports: [RouterOutlet, HttpClientModule, CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
